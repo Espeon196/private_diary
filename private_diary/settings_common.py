0540571cebd102bc95c873d54552ebb91f67cb1b
Torchic255 <z3ijnn3@gmail.com>
2020-12-03 20:44:33 +0900

    第９章完了

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'diary.apps.DiaryConfig',
+    'accounts.apps.AccountsConfig',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
 ]
 
 MIDDLEWARE = [
@@ -104,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
